e08b90544cc7eb2fa13080c12398ab2e5117f5df
rob01010101 <rcpineda.student@ua.edu.ph>
2025-02-13 23:59:18 +0800

    initial-commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+      <h1 className="name">Robby Pineda</h1>
+      <div className="box">
+        <p>CPEITEL</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
